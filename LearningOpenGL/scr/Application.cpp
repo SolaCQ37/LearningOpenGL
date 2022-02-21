f5037aa773d86aeff364fe615d1a39fc24ef9c38
sola <solaalus@163.com>
2022-02-21 21:37:57 +0800

    Use uniform to control color in Basic.shader

--- LearningOpenGL/scr/Application.cpp
+++ LearningOpenGL/scr/Application.cpp
@@ -38,11 +38,43 @@ void APIENTRY debugMessageCallback(
     const GLchar* message,
     const void* userParam)
 {
-    std::cout << "[Source] " << source << ", "
-              << "[Type] " << type << ", "
-              << "[ID] " << id << ", "
-              << "[Severity] " << severity << ", "
-              << "[Message] " << message << std::endl;
+    // Ignore some not important error or warning
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
 }
 
 struct ShaderProgramSource
@@ -216,6 +248,14 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexShader, source.FragmentShader);
     glUseProgram(shader);
 
+    /* Get "u_Color" location in shader */
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != -1);
+    /* Create a uniform */
+    glUniform4f(location, 0.2f, 0.3f, 0.7f, 1.0f);
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -235,7 +275,18 @@ int main(void)
          * type: Specifies the type of the values in indices(must be unsigned)
          * indices: Specifies an offset of the first index in the array in the data
          */
-        glDrawElements(GL_TRIANGLES, 6, GL_INT, NULL);  // Test error handling
+        glUniform4f(location, r, 0.3f, 0.7f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);  // Test error handling
+
+        if (r >= 1.0f)
+        {
+            increment = -0.05f;
+        }
+        else if (r <= 0.0f)
+        {
+            increment = 0.05f;
+        }
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
